28d04f8d0be8bcf72b5af20e80e5f282b9ecd5a5
刘建广 <liujga@enn.cn>
2018-12-06 16:56:29 +0800

    add superratingbar

--- app/src/main/java/com/ljg/superratingbar/MainActivity.java
+++ app/src/main/java/com/ljg/superratingbar/MainActivity.java
@@ -2,12 +2,40 @@ package com.ljg.superratingbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+import com.ljg.ratingbarlib.SuperRatingBar;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private SuperRatingBar mRatingBars;
+    private SuperRatingBar star_have_click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initData();
     }
+
+    private void initView() {
+
+        mRatingBars = findViewById(R.id.star);
+        star_have_click = findViewById(R.id.star_have_click);
+    }
+
+    private void initData() {
+        // mRatingBar.setClickable(false);
+
+        star_have_click.setOnRatingChangeListener(new SuperRatingBar.OnRatingChangeListener() {
+            @Override
+            public void onRatingChange(float ratingCount) {
+                Toast.makeText(getApplicationContext(), ratingCount + "", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
